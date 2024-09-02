593cba4724d144e2457e4ae071ee3d5361ef618e
Jordan Shayo <jordanhildebrand2012@gmail.com>
2024-09-02 12:06:42 +0100

    working with directives, highlighing text in the seatch area

--- start/apps/chapter02/ng-attribute-directive/src/app/app.component.ts
+++ start/apps/chapter02/ng-attribute-directive/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { HeaderComponent } from '@codewithahsan/ng-cb-ui';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HighlightDirective } from './highlight.directive';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
   styleUrls: ['./app.component.scss'],
-  imports: [HeaderComponent, CommonModule],
+  imports: [HeaderComponent, CommonModule, FormsModule, HighlightDirective],
 })
-export class AppComponent {}
+export class AppComponent {
+  searchText = '';
+}
